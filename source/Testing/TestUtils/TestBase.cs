9f8e085946c942dc7d89433691ef070a4bf8499e
Brian Shipe <bmshipe@gmail.com>
2014-12-28 17:09:30 -0600

    Adding base TestUtils project

---
+++ source/Testing/TestUtils/TestBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoubleIPA.Testing.TestUtils
+{
+    /// <summary>
+    /// Base class for unit testing. Provides common helper methods
+    /// </summary>
+    public class TestBase
+    {
+    }
+}
