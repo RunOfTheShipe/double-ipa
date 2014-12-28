943d53fea42c18cfa020736f487b50da68de5700
Brian Shipe <bmshipe@gmail.com>
2014-12-28 17:23:49 -0600

    Added unit tests for Arg class

---
+++ source/Testing/TestUtils/ArgTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using DoubleIPA.Utils;
+using NUnit.Framework;
+
+namespace DoubleIPA.Testing.TestUtils
+{
+    [TestFixture]
+    public class ArgTests : TestBase
+    {
+        [Test]
+        public void Arg_NotNull()
+        {
+            object obj = null;
+
+            Assert.Catch<ArgumentNullException>(() => {
+                Arg.NotNull(obj, "test1");
+            });
+
+            obj = new object();
+            Arg.NotNull(obj, "test2");
+        }
+
+        [Test]
+        public void Arg_Cond()
+        {
+            Assert.Catch<ArgumentException>(() => {
+                Arg.Cond(false);
+            });
+
+            Assert.Catch<ArgumentException>(() => {
+                Arg.Cond(false, "message!");
+            });
+
+            Arg.Cond(true);
+            Arg.Cond(true, "message!");
+        }
+
+        [Test]
+        public void Arg_NotEmpty()
+        {
+            Assert.Catch<ArgumentException>(() => {
+                Arg.NotEmpty<int>(new List<int>(), "test1");
+            });
+
+            Assert.Catch<ArgumentException>(() => {
+                Arg.NotEmpty<object>(null, "test2");
+            });
+
+            List<double> list = new List<double>() { 0.0 };
+            Arg.NotEmpty<double>(list, "test3");
+        }
+    }
+}
